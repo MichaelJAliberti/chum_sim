47275a0dc4c1b0f89842cdbcf322e20b5235936f
MichaelJAliberti <mjali@bu.edu>
2022-04-18 22:23:01 -0400

    scratch file out of project

--- app.ts
+++
@@ -1,8 +0,0 @@
-import { Point } from './src/movement/point'
-import { Attribute } from './src/attributes/attribute'
-
-const pt = new Point(1, 2)
-console.log(`(${pt.x}, ${pt.y})`)
-
-const str = new Attribute('Strength')
-console.log(str.check_cost())
