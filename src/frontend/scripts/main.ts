0f576405e6be6c0f5078a947af55fec8d7e634b3
MichaelJAliberti <mjali@bu.edu>
2022-04-21 00:46:08 -0400

    cleaning comments

--- src/frontend/scripts/main.ts
+++ src/frontend/scripts/main.ts
@@ -2,7 +2,6 @@ import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 
 function createWindow() {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
@@ -10,7 +9,6 @@ function createWindow() {
         show: false,
     })
 
-    // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, '../templates/index.html'))
     console.log(__dirname)
 
@@ -21,22 +19,14 @@ function createWindow() {
     mainWindow.show()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
     createWindow()
 
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
